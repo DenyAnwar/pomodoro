3c157e5dc09f519e6eaa333ff463e41c7c4d87ff
denyanwardev <denyanwardev@gmail.com>
2023-11-14 16:26:35 +0800

    disable landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:myth_green_pomodoro_app/screens/timer_service.dart';
+import 'package:flutter/services.dart';
 
 import 'screens/home_screen.dart';
 
@@ -14,6 +15,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
